3835c3e41c71bf2613786ac9afc491190964d530
cdzhx <cdzhx@outlook.com>
2025-05-25 09:32:05 +0800

    Update hbb_common submodule

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["re.caiu.cn"];
+pub const RS_PUB_KEY: &str = "ZRuKozK8gwuTpnoPR6Pb6akOL+q9hWlLPtnwQg1iibU=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
